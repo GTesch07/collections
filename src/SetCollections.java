9a30d35e7da9985d69f1853c4dfbcab0ae45a403
Guilherme da Silva Tesch <tesch.guilherme@hotmail.com>
2025-08-04 17:06:49 -0300

    feat: add news class.

---
+++ src/SetCollections.java
@@ -0,0 +1,5 @@
+package tesch;
+
+public class SetCollections {
+
+}
---
+++ src/tesch/guilherme/models/User.java
@@ -0,0 +1,45 @@
+package tesch.guilherme.models;
+
+public class User {
+    private int id;
+
+    private String name;
+
+    public User() {
+
+    }
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setname(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "{'id': " + id + ", 'name': '" + name + "}";
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (obj == this)
+            return true;
+        if (!(obj instanceof User))
+            return false;
+    }
+}
